0bd1cc0415316e2f27e971f19e11a966908bd534
andrey_kazakov <aakazakovv@yandex.ru>
2019-12-12 16:41:32 +0300

    done, so-so)

--- hw3/hw3_2.php
+++ hw3/hw3_2.php
@@ -3,7 +3,7 @@
 /* Дан массив из n элементов, начиная с 1. Каждый следующий элемент равен (предыдущий + 1).
  Но в массиве гарантированно 1 число пропущено. Необходимо вывести на экран пропущенное число. */
 
-$arr = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15];
+$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17];
 
 function findMissingNumber(array $arr)
 {
@@ -13,14 +13,14 @@ function findMissingNumber(array $arr)
         $firstNum = $arr[$i];
         $secondNum = $arr[count($arr) - 1 - $i];
         $sum = $firstNum + $secondNum;
+        if ($firstNum > $secondNum) return $firstNum - 1;
         if ($sum !== $controlSum) {
-            var_dump($firstNum, $secondNum, $i);
             if (($firstNum - $arr[$i - 1]) !== 1) return $firstNum - 1;
             if (($arr[count($arr) - $i] - $secondNum) !== 1) return $arr[count($arr) - $i] - 1;
         }
         $i++;
     }
-    return null;
+    return 0;
 }
 
 echo findMissingNumber($arr);
