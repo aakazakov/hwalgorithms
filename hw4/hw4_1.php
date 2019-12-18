603c0379bc3e74f74a59f494e998bcf9ab1f313f
andrey_kazakov <aakazakovv@yandex.ru>
2019-12-18 17:32:34 +0300

    started)

---
+++ hw4/hw4_1.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+    1. объект класса принимает выражение
+    2. выражение разбивается в массив
+    3. строится дерево
+    4. вычисление
+    5. вывод результата
+
+
+    class Calculator( string $mathExpression)
+
+*/
+
+class Calculator
+{
+    private TreeBuilder $tree;
+
+    public function __construct(string $mathException)
+    {
+       $this->tree = new TreeBuilder($mathException);
+    }
+
+    public function getResult()
+    {
+        return 'result';
+    }
+}
+
+class TreeBuilder
+{
+    private string $mathException;
+
+    public function __construct(string $mathException)
+    {
+        $this->mathException = $mathException;
+    }
+
+    private function buildTree()
+    {
+
+    }
+
+    private function getChars(string $str) : array
+    {
+        return preg_split('//', $str, -1, PREG_SPLIT_NO_EMPTY);
+    }
+
+    public function getTree()
+    {
+        return 'tree';
+    }
+}
+
+class FuncLib
+{
+    public static function Priority(string $char)
+    {
+        switch ($char) {
+            case '(':
+            case ')':
+                return 4;
+            case '^':
+                return 3;
+            case '*':
+            case '/':
+                return 2;
+            case '+':
+            case '-':
+                return 1;
+        }
+        return 0;
+    }
+}
+
+//$test = new Calculator('(x+42)^2+7*y-z');
+//print_r($test->getResult());
+//print_r((new TreeBuilder('(x+42)^2+7*y-z'))->mathException);
