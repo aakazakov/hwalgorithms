66f92716d269da29dbc6ab49a2c2fa7e62cbc662
andrey_kazakov <aakazakovv@yandex.ru>
2019-12-19 11:23:09 +0300

    added new classes

--- hw4/hw4_1.php
+++ hw4/hw4_1.php
@@ -2,7 +2,7 @@
 
 class Calculator
 {
-    private array $tree;
+    private BinaryTree $tree;
 
     public function __construct(string $mathException)
     {
@@ -19,10 +19,12 @@ class Calculator
 class TreeBuilder
 {
     private string $mathException;
+    private BinaryTree $binTree;
 
     public function __construct(string $mathException)
     {
         $this->mathException = $mathException;
+        $this->binTree = new BinaryTree();
     }
 
     public function getTree()
@@ -32,12 +34,67 @@ class TreeBuilder
 
     private function buildTree()
     {
-        return $this->getChars($this->mathException);
+        $chars = $this->getChars($this->mathException);
+        foreach ($chars as $value) {
+            $this->binTree->insert($value);
+        }
+        return $this->binTree;
     }
 
     private function getChars(string $str) : array
     {
         return preg_split('//', str_replace(' ', '', $str), -1, PREG_SPLIT_NO_EMPTY);
+    }
+}
+
+class BinaryNode
+{
+    public int $value;
+    public $left;
+    public $right;
+
+    public function __construct(int $value)
+    {
+        $this->value = $value;
+        $this->left = null;
+        $this->right = null;
+
+    }
+}
+
+class BinaryTree
+{
+    private $root;
+
+    public function __construct()
+    {
+        $this->root = null;
+    }
+
+    public function insert($item) : void
+    {
+
+        $node = new BinaryNode($item);
+        if ($this->root === null) {
+            $this->root = $node;
+        } else {
+            $this->insertNode($node, $this->root);
+        }
+    }
+
+    private function insertNode($node, &$subTree) : void
+    {
+        try {
+            if ($subTree === null) $subTree = $node;
+            if ($subTree->value < $node->value) {
+                $this->insertNode($node, $subTree->right);
+            } elseif ($subTree->value > $node->value) {
+                $this->insertNode($node, $subTree->left);
+            }
+        } catch (Error $exception) {
+            print_r($exception->getMessage());
+        }
+
     }
 }
 
@@ -53,12 +110,13 @@ class Lib
         '-' => 1,
     ];
 
-    public static function getPriority(string $char)
+    public static function getPriority(string $char) : int
     {
         return static::$priority[$char] ?: 0;
     }
 }
 
-$test = new Calculator('(2 + 1)^2 + 7*3 - 5');
+//$test = new Calculator('(2 + 1)^2 + 7*3 - 5');
+$test = new Calculator('5376429');
 print_r($test->getResult());
 //print_r((new TreeBuilder('(x+42)^2+7*y-z'))->mathException);
