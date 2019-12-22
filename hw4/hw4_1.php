2572aa67b1e207322a5ad146420b36bb507f7959
andrey_kazakov <aakazakovv@yandex.ru>
2019-12-22 22:13:35 +0300

    prototype done

--- hw4/hw4_1.php
+++ hw4/hw4_1.php
@@ -1,228 +1,148 @@
 <?php
-//
-///*
-//class Calculator
-//{
-//    private BinaryTree $tree;
-//
-//    public function __construct(string $mathException)
-//    {
-//        $this->tree = (new TreeBuilder($mathException))->getTree();
-//    }
-//
-//    public function getResult()
-//    {
-//        print_r('tree traversal result: ' . PHP_EOL);
-//        print_r($this->tree);
-//    }
-//}
-//
-//class TreeBuilder
-//{
-//    private string $mathException;
-//    private BinaryTree $binTree;
-//
-//    public function __construct(string $mathException)
-//    {
-//        $this->mathException = $mathException;
-//        $this->binTree = new BinaryTree();
-//    }
-//
-//    public function getTree()
-//    {
-//        return $this->buildTree();
-//    }
-//
-//    private function buildTree()
-//    {
-//        $chars = $this->getChars($this->mathException);
-//        foreach ($chars as $value) {
-//            $this->binTree->insert($value);
-//        }
-//        return $this->binTree;
-//    }
-//
-//    private function getChars(string $str) : array
-//    {
-//        return preg_split('//', str_replace(' ', '', $str), -1, PREG_SPLIT_NO_EMPTY);
-//    }
-//}
-//
-//class BinaryNode
-//{
-//    public string $value;
-//    public $left;
-//    public $right;
-//
-//    public function __construct(string $value)
-//    {
-//        $this->value = $value;
-//        $this->left = null;
-//        $this->right = null;
-//    }
-//}
-//
-//class BinaryTree
-//{
-//    private $root;
-//
-//    public function __construct()
-//    {
-//        $this->root = null;
-//    }
-//
-//    public function insert($item) : void
-//    {
-//        $node = new BinaryNode($item);
-//        if ($this->root === null) {
-//            $this->root = $node;
-//        } else {
-//            $this->insertNode($node, $this->root);
-//        }
-//    }
-//
-//    private function insertNode($node, &$subTree) : void
-//    {
-//        try {
-//            if ($subTree === null) $subTree = $node;
-//            if (Lib::getPriority($subTree->value) < Lib::getPriority($node->value)) {
-//                $this->insertNode($node, $subTree->right);
-//            } elseif (Lib::getPriority($subTree->value) > Lib::getPriority($node->value)) {
-//                $this->insertNode($node, $subTree->left);
-//            }
-//        } catch (Error $exception) {
-//            print_r($exception->getMessage());
-//        }
-//    }
-//}
-//
-//class Lib
-//{
-//    public static array $priority = [
-////        '(' => 4,
-////        ')' => 4,
-//        '^' => 3,
-//        '*' => 2,
-//        '/' => 2,
-//        '+' => 1,
-//        '-' => 1,
-//    ];
-//
-//    public static function getPriority(string $char) : int
-//    {
-//        return static::$priority[$char] ?: 0;
-//    }
-//}
-//
-//$test = new Calculator('(2 + 1)^2 + 7*3 - 5');
-////$test = new Calculator('5376429');
-//$test->getResult();
-////print_r((new TreeBuilder('(x+42)^2+7*y-z'))->mathException);
-//*/
-//
-//class Calculator
-//{
-//    private array $data;
-//
-//    public function expression(string $expression) : void
-//    {
-//        $this->data = $this->formatIt($expression);
-//    }
-//
-//    private function formatIt(string $expression) : array
-//    {
-//        return preg_split(
-//            '//', str_replace(' ', '', $expression), -1, PREG_SPLIT_NO_EMPTY
-//        );
-//    }
-//
-//    public function getResult()
-//    {
-//        return $this->letCompute();
-//    }
-//
-//    private function letCompute()
-//    {
-//        $tree = $this->getTree();
-//        return $this->calculate($tree);
-//    }
-//
-//    private function calculate($tree)
-//    {
-//        return $tree;
-//    }
-//
-//    private function getTree()
-//    {
-//        $tree = new BinTree();
-//        $tree->buildTree($this->data);
-//        return $tree;
-//    }
-//
-//    /**
-//     * @return array
-//     */
-//    public function getData(): array  // dev
-//    {
-//        return $this->data;
-//    }
-//}
-//
-//class BinTree
-//{
-//    private $root;
-//
-//    public function __construct()
-//    {
-//        $this->root = null;
-//    }
-//
-//    public function buildTree(array $data) : void
-//    {
-//        foreach ($data as $value) {
-//            $this->insert($value);
-//        }
-//    }
-//
-//    private function insert(string $value) : void
-//    {
-//        $node = new BinNode($value);
-//        if ($this->root === null) {
-//            $this->root = $node;
-//        } else {
-//            $this->insertNode($node, $this->root);
-//        }
-//    }
-//
-//    private function insertNode(BinNode $node, &$subTree) : void
-//    {
-//        if ($subTree === null) {
-//            $subTree = $node;
-//        }
-//        if ($subTree->value < $node->value) {
-//            $this->insertNode($node, $subTree->right);
-//        } elseif ($subTree->value > $node->value) {
-//            $this->insertNode($node, $subTree->left);
-//        }
-//    }
-//}
-//
-//class BinNode
-//{
-//    public string $value;
-//    public $left;
-//    public $right;
-//
-//    public function __construct(string $value)
-//    {
-//        $this->value = $value;
-//        $this->left = null;
-//        $this->right = null;
-//    }
-//}
-//
-//$calc = new Calculator();
-////$calc->expression('(2 + 1)^2 + 7*3 - 5');
-//$calc->expression('53764389');
-////print_r($calc->getData());
-//print_r($calc->getResult());
+
+/* Реализовать построение и обход дерева для математического выражения. */
+// (x+4)^2+7*y-z.
+
+class Calculator
+{
+    private Tree $tree;
+
+    public function insert(string $expression) : void
+    {
+        $this->tree = (new TreeBuilder($expression))->getTree();
+    }
+
+    public function getResult(array $params = null) : int
+    {
+        return $this->doComputed($params);
+    }
+
+    private function doComputed(array $params = null) : int
+    {
+        if ($params) return 0;
+        return $this->tree->compute();
+    }
+}
+
+class TreeBuilder
+{
+    private array $stuff;
+    private int $leftEdge;
+    private int $rightEdge;
+
+    public function __construct(string $expression)
+    {
+        $this->stuff = $this->formatIt($expression);
+        $this->leftEdge = 0;
+        $this->rightEdge = count($this->stuff) - 1;
+    }
+
+    public function getTree() : Tree
+    {
+        return $this->buildTree($this->leftEdge, $this->rightEdge);
+    }
+
+    private function buildTree(int $leftEdge, int $rightEdge) : Tree
+    {
+        if ($leftEdge === $rightEdge) return new TreeLeaf($this->stuff[$rightEdge]);
+        $lowestPriorityItem = [];
+        for ($i = $rightEdge; $i > $leftEdge; $i--) {
+            if (Lib::isOperator($this->stuff[$i])) {
+                if (
+                    empty($lowestPriorityItem)
+                    || $lowestPriorityItem['priority'] > Lib::getPriority($this->stuff[$i])
+                ) {
+                    $lowestPriorityItem['index'] = $i;
+                    $lowestPriorityItem['priority'] = Lib::getPriority($this->stuff[$i]);
+                }
+            }
+        }
+        $treeNode = new TreeNode($this->stuff[$lowestPriorityItem['index']]);
+        $treeNode->right = $this->buildTree($lowestPriorityItem['index'] + 1, $rightEdge);
+        $treeNode->left = $this->buildTree($leftEdge, $lowestPriorityItem['index'] - 1);
+        return $treeNode;
+    }
+
+    private function formatIt(string $expression) : array
+    {
+        return preg_split(
+            '//', str_replace(' ', '', $expression), -1, PREG_SPLIT_NO_EMPTY
+        );
+    }
+}
+
+abstract class Tree
+{
+    public string $value;
+
+    abstract public function compute();
+}
+
+class TreeNode extends Tree
+{
+    public Tree $left;
+    public Tree $right;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function compute() : int
+    {
+        switch ($this->value) {
+            case '+':
+                return $this->left->compute() + $this->right->compute();
+            case '-':
+                return $this->left->compute() - $this->right->compute();
+            case '*':
+                return $this->left->compute() * $this->right->compute();
+            case '/':
+                return $this->left->compute() / $this->right->compute();
+            case '^':
+                return $this->left->compute() ** $this->right->compute();
+        }
+        return null;
+    }
+}
+
+class TreeLeaf extends Tree
+{
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function compute() : int
+    {
+        return (int) $this->value;
+    }
+}
+
+class Lib
+{
+    public static array $operators = ['+', '-', '*', '/', '^'];
+    public static array $priorities = [
+        '+' => 1,
+        '-' => 1,
+        '*' => 2,
+        '/' => 2,
+        '^' => 3,
+    ];
+
+    public static function isOperator(string $item) : bool
+    {
+        return in_array($item, static::$operators);
+    }
+
+    public static function getPriority(string $item) : int
+    {
+        return static::$priorities[$item];
+    }
+}
+
+// =========================== //
+$calc = new Calculator();
+//$calc->insert('(x + 4)^2 + 7*y - z');
+$calc->insert('3 + 4^2 + 7*5 - 9');
+print_r($calc->getResult());
--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
 
-include 'hw4/hw4_1new.php';
-
-//===========================
-$calc = new Calculator();
-
-$calc->insert('(x + 4)^2 + 7*y - z');
-
-//print_r($calc->getResult());
-
-//$calc->insert('1+2+3+4');
-print_r($calc->getTree());
+include 'hw4/hw4_1.php';
