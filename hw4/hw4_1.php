1920a462b11cf7e35669ccd4b81f218c23842668
andrey_kazakov <aakazakovv@yandex.ru>
2019-12-23 15:15:10 +0300

    hw4_1 яdone)

--- hw4/hw4_1.php
+++ hw4/hw4_1.php
@@ -2,16 +2,23 @@
 
 /* Реализовать построение и обход дерева для математического выражения. */
 
+// Собственно калькулятор).
 class Calculator
 {
+    // Построенное дерево.
     private Tree $tree;
+
+    // Массив значений переменных вычисляемого выражения (если они есть).
     public static ?array $params = null;
 
+    // Метод принимает мат. выражение.
     public function insert(string $expression) : void
     {
         $this->tree = (new TreeBuilder($expression))->getTree();
     }
 
+    // Метод возвращает результат вычисления,
+    // принимает массив значений переменных.
     public function getResult(array $params = null) : float
     {
         if ($params) self::$params = $params;
@@ -19,9 +26,14 @@ class Calculator
     }
 }
 
+// Строит дерево.
 class TreeBuilder
 {
+    // Массив лексем.
     private array $stuff;
+
+    // Эти переменные задают область поиска
+    // в массиве $stuff.
     private int $leftEdge;
     private int $rightEdge;
 
@@ -32,28 +44,46 @@ class TreeBuilder
         $this->rightEdge = count($this->stuff) - 1;
     }
 
+    // Возвращает готовое дерево.
     public function getTree() : Tree
     {
         return $this->buildTree($this->leftEdge, $this->rightEdge);
     }
 
+    // Строит дерево из узлов и листьев, получая материал из массива $stuff.
     private function buildTree(int $leftEdge, int $rightEdge) : Tree
     {
+        // Если с обеих сторон скобки, заходим внутрь.
         if ($this->stuff[$leftEdge] === '(' && $this->stuff[$rightEdge] === ')') {
             ++$leftEdge;
             --$rightEdge;
         }
+
+        // Если сошлись на одном элементе, он - лист.
         if ($leftEdge === $rightEdge) return new TreeLeaf($this->stuff[$rightEdge]);
+
+        // Сюда попадает элемент массива $stuff с наименьшим приоритетом (в данной области поиска).
         $lowestPriorityItem = [];
+
+        // Проходим массив справа - налево, чтобы найти крайний правый элемент с наименьшим приоритетом,
+        // он будет элементом дерева.
         for ($i = $rightEdge; $i > $leftEdge; $i--) {
+
+            // Если дошли до скобки, идем с другого конца до открывающей скобки,
+            // ищем крайний правый элемент с наименьшим приоритетом.
             if ($this->stuff[$i] === ')') {
                 for ($j = $leftEdge; $j < $rightEdge; $j++) {
+
+                    // Дошли до открывающей скобки - Ок, собираем элемент дерева из того,
+                    // что лежит в массиве $lowestPriorityItem. Идем дальше...
                     if ($this->stuff[$j] === '(') {
                         $treeNode = new TreeNode($this->stuff[$lowestPriorityItem['index']]);
                         $treeNode->right = $this->buildTree($lowestPriorityItem['index'] + 1, $rightEdge);
                         $treeNode->left = $this->buildTree($leftEdge, $lowestPriorityItem['index'] - 1);
                         return $treeNode;
                     }
+
+                    // Определяем, что положить в массив $lowestPriorityItem (когда идем слева - направо).
                     if (Lib::isOperator($this->stuff[$j])) {
                         if (
                             empty($lowestPriorityItem)
@@ -65,6 +95,8 @@ class TreeBuilder
                     }
                 }
             }
+
+            // Определяем, что положить в массив $lowestPriorityItem (когда идем справа - налево).
             if (Lib::isOperator($this->stuff[$i])) {
                 if (
                     empty($lowestPriorityItem)
@@ -75,6 +107,8 @@ class TreeBuilder
                 }
             }
         }
+
+        // Получаем элемент дерева.
         $treeNode = new TreeNode($this->stuff[$lowestPriorityItem['index']]);
         $treeNode->right = $this->buildTree($lowestPriorityItem['index'] + 1, $rightEdge);
         $treeNode->left = $this->buildTree($leftEdge, $lowestPriorityItem['index'] - 1);
@@ -102,6 +136,8 @@ class TreeBuilder
 //        return $treeNode;
 //    }
 
+    // Метод приводит полученную строку с мат. выражением к массиву лексем,
+    // выделяя многозначные и дробные числа.
     private function formatIt(string $expression) : array
     {
         $expression = str_split(str_replace(' ', '', $expression));
@@ -121,6 +157,7 @@ class TreeBuilder
     }
 }
 
+// Дерево).
 abstract class Tree
 {
     public string $value;
@@ -128,8 +165,10 @@ abstract class Tree
     abstract public function compute();
 }
 
+// Узел дерева.
 class TreeNode extends Tree
 {
+    // Боковые элементы узла.
     public Tree $left;
     public Tree $right;
 
@@ -138,6 +177,7 @@ class TreeNode extends Tree
         $this->value = $value;
     }
 
+    // Метод производит мат. операцию между боковыми элементами.
     public function compute() : float
     {
         switch ($this->value) {
@@ -156,6 +196,7 @@ class TreeNode extends Tree
     }
 }
 
+// Лист дерева.
 class TreeLeaf extends Tree
 {
     public function __construct(string $value)
@@ -163,6 +204,9 @@ class TreeLeaf extends Tree
         $this->value = $value;
     }
 
+    // Метод возвращает значение, если оно изначально число,
+    // либо обращается к Калькулятору за значением для переменной
+    // и возвращает его.
     public function compute() : float
     {
         if (!is_numeric($this->value)) {
@@ -172,6 +216,7 @@ class TreeLeaf extends Tree
     }
 }
 
+// Библиотека сопутствующих методов.
 class Lib
 {
     public static array $operators = ['+', '-', '*', '/', '^'];
@@ -196,7 +241,5 @@ class Lib
 
 // =========================== //
 $calc = new Calculator();
-//$calc->insert('x + 4^2 + 7*y - z + 15 - 10.5');
-$calc->insert('x + 4^2 + 7*(z - y) + 15 - 10.5');
-
+$calc->insert('(x + 4^2 + 7*(z - y))/2 + 15 - 10.5'); // 28
 print_r($calc->getResult(['x' => 3, 'y' => 5, 'z' => 9]));
